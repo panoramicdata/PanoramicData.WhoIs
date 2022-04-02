8044acfc654b45493c18fa439d6103628f995ab3
Lorna <68371985+geckogeckogecko@users.noreply.github.com>
2022-04-02 16:21:45 +0100

    Cleaned up code

---
+++ EmailLookup/Program.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Extensions.Configuration;
+using Sparkle.LinkedInNET;
+using System.Net.Mail;
+using Whois;
+
+namespace EmailLookup
+{
+    public class Program
+    {
+        private static async Task<WhoisResponse> GetWhoIsResponseAsync(string domain) 
+            => await new WhoisLookup().LookupAsync(domain).ConfigureAwait(false);
+
+        public static async Task Main()
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: true)
+                .Build();
+            var LIKEY = configuration["AppSettings:LINKEDIN_API_KEY"];
+            var LISECRET = configuration["AppSettings:LINKEDIN_API_SECRET"];
+
+            var linkedinConfig = new LinkedInApiConfiguration(LIKEY, LISECRET);
+
+            MailAddress mailAddress;
+
+            while (true)
+            {
+                Console.WriteLine("Please enter a valid email address.");
+                var inputtedEmail = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(inputtedEmail))
+                {
+                    Console.WriteLine("Please input something!");
+                    continue;
+                }
+                
+                try
+                {
+                    mailAddress = new MailAddress(inputtedEmail.Trim());
+                    break;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Invalid email format - please try again.");
+                }
+            }
+
+            var whoIsResponse = await GetWhoIsResponseAsync(mailAddress.Host)
+                .ConfigureAwait(false);
+
+            Console.WriteLine(whoIsResponse.Content);
+
+            var linkedinapi = new LinkedInApi(linkedinConfig);
+        }
+    }
+}
