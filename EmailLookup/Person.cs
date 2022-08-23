7542fa512648591781be8079ec1acf98a1c99aec
Lorna <68371985+geckogeckogecko@users.noreply.github.com>
2022-08-23 15:57:43 +0100

    Implementing IPersonSearcher interfaces

--- EmailLookup/Person.cs
+++ EmailLookup/Person.cs
@@ -4,27 +4,22 @@ namespace EmailLookup
 {
    public class Person
    {
-	  public Person(string address)
-	  {
-		 MailAddressEmail = new MailAddress(address);
-		 string user = MailAddressEmail.User;
-		 Email = address;
-		 FirstName = user[..user.IndexOf(".", StringComparison.Ordinal)];
-		 LastName = user[(user.IndexOf(".", StringComparison.Ordinal) + 1)..];
-		 CompanyName = MailAddressEmail.Host[..MailAddressEmail.Host.IndexOf(".", StringComparison.Ordinal)];
-		 Domain = MailAddressEmail.Host;
-	  }
+		public Person(string address)
+		{
+			MailAddressEmail = new MailAddress(address);
+			string user = MailAddressEmail.User;
+			Email = address;
+			FirstName = user[..user.IndexOf(".", StringComparison.Ordinal)];
+			LastName = user[(user.IndexOf(".", StringComparison.Ordinal) + 1)..];
+			CompanyName = MailAddressEmail.Host[..MailAddressEmail.Host.IndexOf(".", StringComparison.Ordinal)];
+			Domain = MailAddressEmail.Host;
+		}
 
-	  public string FirstName { get; }
-
-	  public string LastName { get; }
-
-	  public string CompanyName { get; }
-
-	  public string Domain { get; }
-
-      public string Email { get; }
-
-	  public MailAddress MailAddressEmail { get; }
+	public string FirstName { get;  }
+	public string LastName { get; }
+	public string CompanyName { get; }
+	public string Domain { get; }
+	public string Email { get; }
+	public MailAddress MailAddressEmail { get; }
    }
 }
