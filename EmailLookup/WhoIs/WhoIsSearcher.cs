11cbc46471cf6f71ab2f12b2f322f651921144eb
Lorna Harvey <lorna_harvey@hotmail.com>
2022-08-24 17:03:38 +0100

    WhoIsSearcher now uses the IPersonSearcher interface

--- EmailLookup/WhoIs/WhoIsSearcher.cs
+++ EmailLookup/WhoIs/WhoIsSearcher.cs
@@ -2,8 +2,20 @@
 
 namespace EmailLookup
 {
-	public class WhoIsSearcher
+	public class WhoIsSearcher : IPersonSearcher
 	{
+		public async Task<Profile?> SearchAsync(Person person)
+		{
+			CancellationToken cancellationToken = default;
+			var domain = person.Domain;
+
+			var response = await new WhoisLookup()
+				.LookupAsync(domain)
+				.ConfigureAwait(false);
+
+			return null;
+		}
+		
 		public async Task<WhoisResponse?> GetResponseAsync(
 		   string domain,
 		   CancellationToken cancellationToken
